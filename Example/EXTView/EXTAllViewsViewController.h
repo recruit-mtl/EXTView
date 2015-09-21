f1da39872b42e7cc20f3b018a2f1efd6dc197650
MORITA Hideyuki <emeitch@emeitch.com>
2015-09-21 17:52:33 +0900

    arrange storyboards

---
+++ Example/EXTView/EXTAllViewsViewController.h
@@ -0,0 +1,13 @@
+//
+//  EXTAllViewsViewController.h
+//  EXTView
+//
+//  Created by emeitch on 9/21/15.
+//  Copyright © 2015 MORITA Hideyuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EXTAllViewsViewController : UIViewController <UICollectionViewDataSource>
+
+@end
---
+++ Example/EXTView/EXTAllViewsViewController.m
@@ -0,0 +1,57 @@
+//
+//  EXTAllViewsViewController.m
+//  EXTView
+//
+//  Created by emeitch on 9/21/15.
+//  Copyright © 2015 MORITA Hideyuki. All rights reserved.
+//
+
+#import "EXTAllViewsViewController.h"
+
+@interface EXTAllViewsViewController ()
+
+@end
+
+@implementation EXTAllViewsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    return 1;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 0;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return nil;
+}
+
+- (IBAction)closeAction:(id)sender
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+@end
